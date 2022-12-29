4212881c08a97cb8cea7b7adeded8e5428c1e419
sangramb7 <sbtungsten76@gmail.com>
2022-12-29 19:37:30 +0530

    DOM Task 3

---
+++ dom.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 2px black';
+
+var title = document.getElementsByClassName('title');
+
+title[0].style.fontWeight = 'bold';
+title[0].style.color = 'green';
