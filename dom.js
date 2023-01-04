213f61b6e5cbdac3172aa337ae048e55161f6b85
sangramb7 <sbtungsten76@gmail.com>
2023-01-04 21:35:09 +0530

    Task 6

--- dom.js
+++ dom.js
@@ -7,7 +7,7 @@ var title = document.getElementsByClassName('title');
 title[0].style.fontWeight = 'bold';
 title[0].style.color = 'green';
 
-var items = document.getElementsByTagName('li');
+/* var items = document.getElementsByTagName('li');
 
 items[1].textContent = 'Hello 2';
 items[1].style.backgroundColor = 'yellow';
@@ -16,4 +16,9 @@ for(var i=0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
 }
 
-items[2].style.backgroundColor = 'green';
+items[2].style.backgroundColor = 'green'; */
+
+var items = document.querySelectorAll('li');
+
+items[1].style.backgroundColor = 'green';
+items[2].style.visibility = 'hidden';
