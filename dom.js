97a874fbb0033e829a1b7149a020a761a8d509f4
sangramb7 <sbtungsten76@gmail.com>
2023-01-04 21:17:31 +0530

    Task 5

--- dom.js
+++ dom.js
@@ -7,7 +7,7 @@ var title = document.getElementsByClassName('title');
 title[0].style.fontWeight = 'bold';
 title[0].style.color = 'green';
 
-var items = document.getElementsByClassName('list-group-item');
+var items = document.getElementsByTagName('li');
 
 items[1].textContent = 'Hello 2';
 items[1].style.backgroundColor = 'yellow';
